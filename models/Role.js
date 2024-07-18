db351c3a5c6a16f1ce163cb2683840cf935c491e
Carlos Santos <karscx@gmail.com>
2024-07-18 00:11:56 -0300

    Fix

--- models/Role.js
+++ models/Role.js
@@ -19,6 +19,11 @@ module.exports = (sequelize) => {
 
   Role.init(
     {
+      id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        primaryKey: true,
+      },
       name: {
         type: DataTypes.STRING,
         allowNull: false,
